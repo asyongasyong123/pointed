17a44cbdec5c599f4078f6530d11d148911daa51
asyongasyong123 <asyongasyong123@gmail.com>
2026-07-12 23:26:24 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 set -e
 
-# ✅ Siguroha nga naay permiso
-chmod +x /usr/local/bin/xray
-
-# Sugdi ang Xray sa likod
 /usr/local/bin/xray run -c /etc/xray.json &
-XRAY_PID=$!
-
-# Hulata kadiyot aron masugdan ang Xray
 sleep 3
-
-# Sugdi ang Nginx sa foreground
 exec nginx -g 'daemon off;'
