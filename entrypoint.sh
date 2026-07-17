fdda8f9745f92fe4391f144cdb5cdbe614e724cf
asyongasyong123 <asyongasyong123@gmail.com>
2026-07-17 10:57:17 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Ilisi ang variable sa sulod sa config
+sed -i "s|env:POINTED_IP|${POINTED_IP}|g" /etc/xray/config.json
+# Sugdi ang Xray
 xray run -c /etc/xray/config.json &
 sleep 2
 nginx -g 'daemon off;'
